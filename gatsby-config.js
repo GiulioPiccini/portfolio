b35ef2e3be259990fe665ad66b0068dc90946daa
NicolaRamazzotti <nicorama15@gmail.com>
2020-11-23 12:11:13 +0100

    offline plugin update

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-offline`,
       options: {
-        precachePages: [`/about/`, `/index/*`, `works/*`],
+        precachePages: [`/about/`, `/index/`, `works/*`],
       },
     },
   ],

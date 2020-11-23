f675a5c9688f1c04832593c43ca28b0a8efac0b4
NicolaRamazzotti <nicorama15@gmail.com>
2020-11-23 12:26:55 +0100

    manifest installed

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,11 +17,25 @@ module.exports = {
         apiToken: process.env.DATO_API_TOKEN,
       },
     },
-    // Plugin off line
+    // Off line
     {
       resolve: `gatsby-plugin-offline`,
       options: {
         precachePages: [`/about/`, `/index/`, `works/*`],
+      },
+    },
+    // Manifest
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Creative Portfolio",
+        short_name: "CP",
+        start_url: "/",
+        background_color: "#383838",
+        theme_color: "#383838",
+        display: "standalone",
+        icon: "src/images/favicon/favicon.ico",
+        crossOrigin: `use-credentials`,
       },
     },
   ],

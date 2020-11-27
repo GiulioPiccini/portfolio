dbf346673f6942b8713fe270a121daf3b5912753
NicolaRamazzotti <nicorama15@gmail.com>
2020-11-27 19:26:54 +0100

    manifest deleted

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,21 +5,6 @@ module.exports = {
     title: `Creative Portfolio`,
   },
   plugins: [
-    // Manifest ** Need to declare this plugin before gatsby-plugin-offline for work correctly ** NEED TO ADD ICON AND ICONS
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Creative Portfolio`,
-        short_name: `CP`,
-        description: `The application does cool things and makes your life better.`,
-        lang: `en`,
-        start_url: process.env.PATH_PREFIX,
-        background_color: `#383838`,
-        theme_color: `#383838`,
-        display: `standalone`,
-        icon: `./public/favicon-32x32.png`,
-      },
-    },
     `gatsby-plugin-preload-fonts`,
     `gatsby-plugin-preact`,
     `gatsby-plugin-react-helmet`,
